32f91436fd13234599c542da24bc6ed557647c0c
Aline Naoe <alinenaoe@gmail.com>
2023-05-10 22:23:51 -0300

    ex 4: check guess letters

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
+  const lettersCheck = checkGuess(value, answer);
+
+  const getCellClass = (number) => {
+    if (!value) return;
+    return lettersCheck[number].status;
+  };
+
   return (
     <p className="guess">
       {range(5).map((number) => (
-        <span className="cell" key={number}>
+        <span className={`cell ${getCellClass(number)}`} key={number}>
           {value ? value[number] : undefined}
         </span>
       ))}
